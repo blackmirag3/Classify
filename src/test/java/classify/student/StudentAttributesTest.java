2f314df53044e22f06911f7b2537d9d181d89acf
tayponghee <tayponghee@gmail.com>
2024-03-29 16:20:08 +0800

    Fix Gradle checks

--- src/test/java/classify/student/StudentAttributesTest.java
+++ src/test/java/classify/student/StudentAttributesTest.java
@@ -2,7 +2,9 @@ package classify.student;
 
 import org.junit.jupiter.api.Test;
 import java.util.List;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StudentAttributesTest {
 
