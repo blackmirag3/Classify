20676e56c1278868db5e8a2364aa3d0a4dfcd639
Shui Hon Siew <shuihon@gmail.com>
2024-04-03 00:21:50 +0800

    Add javadoc for archive commands

--- src/main/java/classify/commands/ArchiveCommands.java
+++ src/main/java/classify/commands/ArchiveCommands.java
@@ -9,36 +9,67 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ArchiveCommands {
+
     //@@author blackmirag3
+    /***
+     * Prompts for student name to search in archive list
+     * If student exists, this method removes student from archive list to add to master list
+     * Writes archive and student information file in data directory
+     *
+     * @param masterList ArrayList of Students who are in master list
+     * @param archiveList ArrayList of Students who are archived
+     * @param name String for student's name
+     * @param in Scanner to read user input
+     */
     public static void unarchiveStudent(ArrayList<Student> masterList, ArrayList<Student> archiveList,
                                         String name, Scanner in) {
-        if (name == null) {
-            name = Commands.promptName(in);
-        }
-        Student student = InputParsing.findStudentByName(archiveList, name, in);
+
+        Student student = getStudent(archiveList, name, in);
+
         if (student == null) {
-            UI.printStudentNotFound();
             return;
         }
+
         archiveList.remove(student);
         masterList.add(student);
         DataHandler.writeArchive(archiveList);
         DataHandler.writeStudentInfo(masterList);
     }
 
+    /***
+     * Prompts for student name to search in master student list
+     * If student exists, this method removes student from master list to add to archive list
+     * Writes archive and student information file in data directory
+     *
+     * @param masterList ArrayList of Students who are in master list
+     * @param archiveList ArrayList of Students who are archived
+     * @param name String for student's name
+     * @param in Scanner to read user input
+     */
     public static void archiveStudent(ArrayList<Student> masterList, ArrayList<Student> archiveList,
                                       String name, Scanner in) {
-        if (name == null) {
-            name = Commands.promptName(in);
-        }
-        Student student = InputParsing.findStudentByName(masterList, name, in);
+
+        Student student = getStudent(masterList, name, in);
+
         if (student == null) {
-            UI.printStudentNotFound();
             return;
         }
+
         masterList.remove(student);
         archiveList.add(student);
         DataHandler.writeArchive(archiveList);
         DataHandler.writeStudentInfo(masterList);
+    }
+
+    private static Student getStudent(ArrayList<Student> archiveList, String name, Scanner in) {
+        if (name == null) {
+            name = Commands.promptName(in);
+        }
+        Student student = InputParsing.findStudentByName(archiveList, name, in);
+        if (student == null) {
+            UI.printStudentNotFound();
+            return null;
+        }
+        return student;
     }
 }
