bdda25f115a1f7e654eeccdfbd5733d2129ff878
Parth Sanjay Gandhi <E0957991@u.nus.edu>
2024-04-03 00:37:06 +0800

    Edit Data Management to support basic attributes

--- src/main/java/classify/data/DataReader.java
+++ src/main/java/classify/data/DataReader.java
@@ -11,6 +11,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.logging.Level;
@@ -35,13 +38,44 @@ public class DataReader {
 
         if (studentFileInput != null){
 
-            Student student = new Student(studentFileInput);
-
+            String [] inputArr = studentFileInput.split("~~");
+            
+            //Set Name
+            Student student = new Student(inputArr[0].trim());
             masterStudentList.add(student);
+
+            //Set Gender
+            student.getAttributes().setGender(inputArr[1].trim());
+            
+            //Set Phone Number
+            try {
+            student.getAttributes().setPhoneNumber(Integer.parseInt(inputArr[2].trim()));
+            } catch (NumberFormatException e) {
+                UI.println("Error parsing the phone number.");
+            }
+
+            //Set Last Payment Date
+            try {
+            LocalDate inputLastPaymentDate = convertStringInput(inputArr[3].trim());
+            student.getAttributes().setLastPaymentDate(inputLastPaymentDate);
+            } catch (DateTimeParseException e) {
+                UI.println("Error parsing the last payment date.");
+            }
+
+            //Set remarks
+            student.getAttributes().setRemarks(inputArr[4].trim());
+            
             logger.log(Level.INFO, "Student added successfully.");
             UI.printStudentAdded();
             UI.printDivider();
         }
+    }
+
+    public static LocalDate convertStringInput(String input) throws DateTimeParseException {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        LocalDate dateToReturn = LocalDate.parse(input,formatter);
+        return dateToReturn;
     }
 
     public static void initialiseData(ArrayList<Student> list,
