4403cb052eba260d5eec84539295a94ffb378f2c
Parth Sanjay Gandhi <E0957991@u.nus.edu>
2024-04-10 04:54:22 +0800

    Clean up TextFileHandler

--- src/main/java/classify/textfilecode/TextFileHandler.java
+++ src/main/java/classify/textfilecode/TextFileHandler.java
@@ -1,5 +1,8 @@
 package classify.textfilecode;
 
+import classify.student.Student;
+import classify.ui.UI;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.FileAlreadyExistsException;
@@ -9,23 +12,21 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import classify.student.Student;
-import classify.ui.UI;
-
 //@@author ParthGandhiNUS
 public class TextFileHandler {
     private static final String CURRENT_FILES = "Current Files in your Input Folder:";
     private static final String INPUT_TEXT_FILE_DIRECTORY = "./data/inputFolder";
-    private static final String IO_EXCEPTION_MESSAGE_FOR_TEXT_FILE = "Something went horribly wrong while creating your data folder!";
-    public static final File currentDirectory = new File (INPUT_TEXT_FILE_DIRECTORY);
+    private static final String IO_EXCEPTION_MESSAGE_FOR_TEXT_FILE = "Something went horribly wrong while creating " +
+            "your data folder!";
+    public static final File CURRENT_DIRECTORY = new File (INPUT_TEXT_FILE_DIRECTORY);
 
-    public static void process(ArrayList<Student> masterStudentList, Scanner in, String textFileName) {
+    public static void process(ArrayList<Student> masterStudentList, Scanner in) {
         createTextFileDirectory();
         // Print out the current files in your folder
         UI.println(CURRENT_FILES);
         TextFileReader.printCurrentInputFolder(INPUT_TEXT_FILE_DIRECTORY);
         TextFileParser.promptForFileSelection();
-        TextFileParser.parseUserSelection(currentDirectory, in);
+        TextFileParser.parseUserSelection(CURRENT_DIRECTORY, in, masterStudentList);
     }
 
     /**
@@ -37,7 +38,7 @@ public class TextFileHandler {
             Path path = Paths.get(INPUT_TEXT_FILE_DIRECTORY);
             Files.createDirectories(path);
         } catch (FileAlreadyExistsException ignored){
-            //Ignore this error as this should not cause any issues, we dont want replicas of the same file
+            //Ignore this error as this should not cause any issues, we don't want replicas of the same file
         } catch (IOException e){
             UI.println(IO_EXCEPTION_MESSAGE_FOR_TEXT_FILE);
         }
