e35211e690daa8d4e49de61acb87e1150b67d812
Parth Sanjay Gandhi <E0957991@u.nus.edu>
2024-04-10 21:22:55 +0800

    Make changes according to points raised during review

--- src/main/java/classify/textfilecode/TextFileParser.java
+++ src/main/java/classify/textfilecode/TextFileParser.java
@@ -1,29 +1,22 @@
 package classify.textfilecode;
 
-import classify.data.DataReader;
 import classify.student.Student;
-import classify.student.StudentList;
 import classify.student.SubjectGrade;
-import classify.ui.DataUI;
 import classify.ui.UI;
 import classify.user.InputParsing;
 import classify.user.InvalidCharacterException;
-import classify.user.NameNumberMatchException;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.time.LocalDate;
-import java.time.format.DateTimeParseException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 //@@author ParthGandhiNUS
 public class TextFileParser {
     private static final String MAIN_REGEX = "~~";
-    private static final String SUBJECT_INFO_REGEX = "##";
     private static final String FILE_SELECTION_PROMPT = "Please enter the exact name of the file " +
             "you'd like to process:";
     private static final String REQUEST_TO_TRY_AGAIN = "Please try again!";
@@ -32,13 +25,7 @@ public class TextFileParser {
     private static final String NO_MATCH_FOUND = "No Match Found!";
     private static final String ERROR_ACCESSING_THE_FILE = "Something went wrong while accessing the file";
     private static final Integer NAME = 0;
-    private static final Integer GENDER = 1;
-    private static final Integer PHONE_NUMBER = 2;
-    private static final Integer LAST_PAYMENT_DATE = 3;
-    private static final Integer REMARKS = 4;
-    private static final Integer SUBJECTS = 5;
-    private static final Integer GRADE_FOR_SUBJECT = 0;
-    private static final Integer NUMBER_OF_CLASSES_ATTENDED = 1;
+    private static final Integer GRADE_FOR_SUBJECT = 1;
     private static final String INPUT_TEXT_FILE_DIRECTORY = "./data/inputFolder";
     /**
      * Boolean that checks for the filetype of the files.
@@ -111,9 +98,13 @@ public class TextFileParser {
             String [] subjectInfoStrings = firstLine.split("Subject:");
             String subjectName = subjectInfoStrings[1].trim();
 
+            String secondLine = line.readLine();
+            String [] classInfoStrings = secondLine.split("Classes Attended:");
+            int totalClassesAttended = Integer.parseInt(classInfoStrings[1].trim());
+
             while (line.ready()){
                 String input = line.readLine();
-                decodeTextFile(input, subjectName, masterStudentList);
+                decodeTextFile(input, subjectName, totalClassesAttended, masterStudentList);
             }
         } catch (IOException e) {
             UI.println(ERROR_ACCESSING_THE_FILE);
@@ -121,53 +112,26 @@ public class TextFileParser {
     }
 
     private static void decodeTextFile (String textFileInput, String subjectName,
-                                        ArrayList <Student> masterStudentList){
+                                        Integer classesAttended, ArrayList <Student> masterStudentList){
         if (textFileInput.trim().isBlank()){
             return;
         }
         String [] inputArr = textFileInput.split(MAIN_REGEX);
 
-        //Set Name
-        Student student = new Student(inputArr[NAME].trim());
-        masterStudentList.add(student);
-        //Set Gender
-        student.getAttributes().setGender(inputArr[GENDER].trim());
-        //Set Phone Number
-        try {
 
+        try{
+            //Set Name
+            Student student = new Student(inputArr[NAME].trim());
             InputParsing.checkForSpecialCharacters(student.getName());
-            InputParsing.checkForSpecialCharacters(student.getGender());
-            int phoneNumber = Integer.parseInt(inputArr[PHONE_NUMBER].trim());
-            StudentList.checkNameNumberPair(StudentList.masterStudentList, student.getName(), phoneNumber);
-            StudentList.checkNameNumberPair(StudentList.archiveList, student.getName(), phoneNumber);
-            student.getAttributes().setPhoneNumber(phoneNumber);
-        } catch (NumberFormatException e) {
-            DataUI.phoneNumberParsingError();
-            return;
-        } catch (NameNumberMatchException e) {
-            DataUI.nameNumberPair();
-            return;
+            //Set grades
+            double subjectGrades = Double.parseDouble(inputArr[GRADE_FOR_SUBJECT].trim());
+            //Put Everything together
+            SubjectGrade newSubject = new SubjectGrade(subjectName, subjectGrades, classesAttended);
+            masterStudentList.add(student);
+            student.getAttributes().addSubjectGrade(newSubject);
         } catch (InvalidCharacterException e) {
-            DataUI.invalidCharacterExceptionMessage();
-            return;
+            UI.println("Invalid character found in subject. Skipping subsequent entries.");
         }
-        //Set Last Payment Date
-        try {
-            LocalDate inputLastPaymentDate = DataReader.convertStringInput(inputArr[LAST_PAYMENT_DATE].trim());
-            student.getAttributes().setLastPaymentDate(inputLastPaymentDate);
-        } catch (DateTimeParseException e) {
-            DataUI.lastPaymentDateParseExceptionMessage();
-        }
-        //Set remarks
-        student.getAttributes().setRemarks(inputArr[REMARKS].trim());
-        
-        String [] subjectInformation = inputArr[SUBJECTS].trim().split(SUBJECT_INFO_REGEX);
-        //Set grades
-        double subjectGrades = Double.parseDouble(subjectInformation[GRADE_FOR_SUBJECT].trim());
-        //Set Classes Attended
-        int subjectClassesAttended = Integer.parseInt(subjectInformation[NUMBER_OF_CLASSES_ATTENDED].trim());
-        SubjectGrade newSubject = new SubjectGrade(subjectName, subjectGrades, subjectClassesAttended);
-        student.getAttributes().addSubjectGrade(newSubject);
     }
 
     /**
