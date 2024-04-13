0e8b0cd5367778ca02f19f7fc209d3ad53f87b6c
Cryolian <tanyixin@gmail.com>
2024-04-13 14:38:23 +0800

    Add number and gender tests

--- src/test/java/classify/commands/ViewStudentTest.java
+++ src/test/java/classify/commands/ViewStudentTest.java
@@ -9,6 +9,7 @@ import classify.student.StudentList;
 
 import org.junit.jupiter.api.Test;
 
+
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
@@ -62,5 +63,47 @@ class ViewStudentTest {
 
         assertEquals(expectedOutput, printedOutput);
 
+    }
+
+    @Test
+    void viewMultipleStudentsOfTheSameName() {
+        StudentList studentlist = new StudentList();
+
+        Student alpha = new Student("Alpha");
+        alpha.getAttributes().setPhoneNumber(88888888);
+        studentlist.addStudent(alpha);
+
+        Student alpha2 = new Student("Alpha");
+        alpha2.getAttributes().setPhoneNumber(99999999);
+        studentlist.addStudent(alpha2);
+
+        assertEquals(studentlist.studentList.size(), 2);
+
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        Scanner input = new Scanner("88888888\n");
+
+        System.setOut(new PrintStream(output));
+
+        ViewStudent.viewStudent(studentlist.studentList, input, "alpha");
+        String expectedOutput = "Name found in existing student. " +
+                "Please make sure the entered phone number will be unique." + System.lineSeparator() +
+                "Alpha: 88888888" + System.lineSeparator() +
+                "Alpha: 99999999" + System.lineSeparator() + 
+                "Please input a valid phone number: " + System.lineSeparator() +
+                "Enter a non-number or blank to exit interface." + System.lineSeparator() +
+                "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" + System.lineSeparator() +
+                "Student details: " + System.lineSeparator() +
+                "Name: Alpha" + System.lineSeparator() + System.lineSeparator() + 
+                "Phone Number: 88888888" + System.lineSeparator() +
+                "Gender: null" + System.lineSeparator() +
+                "Last Payment Date: 2024-04-13" + System.lineSeparator() +
+                "Remarks: NA" + System.lineSeparator() +  
+                "No subjects and grades found for this student." + System.lineSeparator() +
+                "Total classes attended across all subjects: 0"; 
+
+        expectedOutput = expectedOutput.trim();
+        String printedOutput = output.toString().trim();
+
+        assertEquals(expectedOutput.length(), printedOutput.length());
     }
 }
